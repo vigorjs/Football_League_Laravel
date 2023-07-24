5d510de640f5d2e15a17f8f45db512ba17ddf633
vigorjs <111201912289@mhs.dinus.ac.id>
2023-07-24 19:06:57 +0700

    update middleware

--- resources/views/pages/admin/user.blade.php
+++ resources/views/pages/admin/user.blade.php
@@ -183,7 +183,7 @@
             <!-- Main Sidebar Container -->
             <aside class="main-sidebar sidebar-dark-primary elevation-4">
                 <!-- Brand Logo -->
-                <a href="index3.html" class="brand-link">
+                <a href="$" class="brand-link">
                 <img src="{{asset('lte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">AdminLTE 3</span>
                 </a>
@@ -196,7 +196,7 @@
                     <img src="{{asset('lte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                    <a href="#" class="d-block">Alexander Pierce</a>
+                    <a href="#" class="d-block">{{Auth::user()->name}}</a>
                     </div>
                 </div>
 
